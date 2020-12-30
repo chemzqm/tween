0d6914b219e03607b2697eae5038259381f6f162
Qiming Zhao <chemzqm@gmail.com>
2020-12-30 18:48:49 +0800

    Release 1.2.1

--- index.js
+++ index.js
@@ -3,16 +3,15 @@
  * Module dependencies.
  */
 
-var Emitter = require('emitter');
-var clone = require('clone');
-var type = require('type');
-var ease = require('ease');
+var Emitter = require('component-emitter')
+var type = require('component-type')
+var ease = require('ease-component')
 
 /**
  * Expose `Tween`.
  */
 
-module.exports = Tween;
+module.exports = Tween
 
 /**
  * Initialize a new `Tween` with `obj`.
@@ -22,17 +21,17 @@ module.exports = Tween;
  */
 
 function Tween(obj) {
-  if (!(this instanceof Tween)) return new Tween(obj);
-  this._from = obj;
-  this.ease('linear');
-  this.duration(500);
+  if (!(this instanceof Tween)) return new Tween(obj)
+  this._from = obj
+  this.ease('linear')
+  this.duration(500)
 }
 
 /**
  * Mixin emitter.
  */
 
-Emitter(Tween.prototype);
+Emitter(Tween.prototype)
 
 /**
  * Reset the tween.
@@ -40,13 +39,13 @@ Emitter(Tween.prototype);
  * @api public
  */
 
-Tween.prototype.reset = function(){
-  this.isArray = 'array' === type(this._from);
-  this._curr = clone(this._from);
-  this._done = false;
-  this._start = Date.now();
-  return this;
-};
+Tween.prototype.reset = function () {
+  this.isArray = 'array' === type(this._from)
+  this._curr = Object.assign({}, this._from)
+  this._done = false
+  this._start = Date.now()
+  return this
+}
 
 /**
  * Tween to `obj` and reset internal state.
@@ -58,11 +57,11 @@ Tween.prototype.reset = function(){
  * @api public
  */
 
-Tween.prototype.to = function(obj){
-  this.reset();
-  this._to = obj;
-  return this;
-};
+Tween.prototype.to = function (obj) {
+  this.reset()
+  this._to = obj
+  return this
+}
 
 /**
  * Set duration to `ms` [500].
@@ -72,10 +71,10 @@ Tween.prototype.to = function(obj){
  * @api public
  */
 
-Tween.prototype.duration = function(ms){
-  this._duration = ms;
-  return this;
-};
+Tween.prototype.duration = function (ms) {
+  this._duration = ms
+  return this
+}
 
 /**
  * Set easing function to `fn`.
@@ -87,12 +86,12 @@ Tween.prototype.duration = function(ms){
  * @api public
  */
 
-Tween.prototype.ease = function(fn){
-  fn = 'function' == typeof fn ? fn : ease[fn];
-  if (!fn) throw new TypeError('invalid easing function');
-  this._ease = fn;
-  return this;
-};
+Tween.prototype.ease = function (fn) {
+  fn = 'function' == typeof fn ? fn : ease[fn]
+  if (!fn) throw new TypeError('invalid easing function')
+  this._ease = fn
+  return this
+}
 
 /**
  * Stop the tween and immediately emit "stop" and "end".
@@ -101,13 +100,13 @@ Tween.prototype.ease = function(fn){
  * @api public
  */
 
-Tween.prototype.stop = function(){
-  this.stopped = true;
-  this._done = true;
-  this.emit('stop');
-  this.emit('end');
-  return this;
-};
+Tween.prototype.stop = function () {
+  this.stopped = true
+  this._done = true
+  this.emit('stop')
+  this.emit('end')
+  return this
+}
 
 /**
  * Perform a step.
@@ -116,50 +115,50 @@ Tween.prototype.stop = function(){
  * @api private
  */
 
-Tween.prototype.step = function(){
-  if (this._done) return;
+Tween.prototype.step = function () {
+  if (this._done) return
 
   // duration
-  var duration = this._duration;
-  var now = Date.now();
-  var delta = now - this._start;
-  var done = delta >= duration;
+  var duration = this._duration
+  var now = Date.now()
+  var delta = now - this._start
+  var done = delta >= duration
 
   // complete
   if (done) {
-    this._from = this._to;
-    this._update(this._to);
-    this._done = true;
-    this.emit('end');
-    return this;
+    this._from = this._to
+    this._update(this._to)
+    this._done = true
+    this.emit('end')
+    return this
   }
 
   // tween
-  var from = this._from;
-  var to = this._to;
-  var curr = this._curr;
-  var fn = this._ease;
-  var p = (now - this._start) / duration;
-  var n = fn(p);
+  var from = this._from
+  var to = this._to
+  var curr = this._curr
+  var fn = this._ease
+  var p = (now - this._start) / duration
+  var n = fn(p)
 
   // array
   if (this.isArray) {
     for (var i = 0; i < from.length; ++i) {
-      curr[i] = from[i] + (to[i] - from[i]) * n;
+      curr[i] = from[i] + (to[i] - from[i]) * n
     }
 
-    this._update(curr);
-    return this;
+    this._update(curr)
+    return this
   }
 
   // objech
   for (var k in from) {
-    curr[k] = from[k] + (to[k] - from[k]) * n;
+    curr[k] = from[k] + (to[k] - from[k]) * n
   }
 
-  this._update(curr);
-  return this;
-};
+  this._update(curr)
+  return this
+}
 
 /**
  * Set update function to `fn` or
@@ -171,8 +170,8 @@ Tween.prototype.step = function(){
  * @api public
  */
 
-Tween.prototype.update = function(fn){
-  if (0 == arguments.length) return this.step();
-  this._update = fn;
-  return this;
-};
+Tween.prototype.update = function (fn) {
+  if (0 == arguments.length) return this.step()
+  this._update = fn
+  return this
+}
